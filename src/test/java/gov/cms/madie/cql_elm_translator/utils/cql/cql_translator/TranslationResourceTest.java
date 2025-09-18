dcace3211ebbb0deb5568780bfb5b8ac79082d60
nmorasb <nate.moraca@semanticbits.com>
2025-09-18 12:17:11 -0400

    MAT-9180: refactor TranslationResource to be initialized with an external ModelManager

--- src/test/java/gov/cms/madie/cql_elm_translator/utils/cql/cql_translator/TranslationResourceTest.java
+++ src/test/java/gov/cms/madie/cql_elm_translator/utils/cql/cql_translator/TranslationResourceTest.java
@@ -1,20 +1,58 @@
 package gov.cms.madie.cql_elm_translator.utils.cql.cql_translator;
 
-import org.junit.jupiter.api.Assertions;
-
-import org.junit.jupiter.api.Disabled;
+import org.cqframework.cql.cql2elm.ModelManager;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.*;
 
 class TranslationResourceTest {
-  TranslationResource translationResource = TranslationResource.getInstance(true);
 
-  @Disabled
-  //  @Test
-  void buildTranslator_checkExceptionHandling() {
+  private static ModelManager mockModelManager;
 
-    Assertions.assertThrows(
-        TranslationFailureException.class,
-        () -> {
-          translationResource.buildTranslator(null, null, null);
-        });
+  @BeforeAll
+  static void setUp() {
+    mockModelManager = new ModelManager();
+  }
+
+  @Test
+  void givenModelManagerAndIsFhirTrue_whenGetInstance_thenReturnsFhirTranslationResource() {
+    // Given
+    boolean isFhir = true;
+
+    // When
+    TranslationResource resource = TranslationResource.getInstance(mockModelManager, isFhir);
+
+    // Then
+    MatcherAssert.assertThat(resource, Matchers.notNullValue());
+    MatcherAssert.assertThat(resource.getLibraryManager(), Matchers.notNullValue());
+    MatcherAssert.assertThat(
+        resource.getLibraryManager().getModelManager(), Matchers.sameInstance(mockModelManager));
+  }
+
+  @Test
+  void givenModelManagerAndIsFhirFalse_whenGetInstance_thenReturnsQdmTranslationResource() {
+    // Given
+    boolean isFhir = false;
+
+    // When
+    TranslationResource resource = TranslationResource.getInstance(mockModelManager, isFhir);
+
+    // Then
+    MatcherAssert.assertThat(resource, Matchers.notNullValue());
+    MatcherAssert.assertThat(resource.getLibraryManager(), Matchers.notNullValue());
+    MatcherAssert.assertThat(
+        resource.getLibraryManager().getModelManager(), Matchers.sameInstance(mockModelManager));
+  }
+
+  @Test
+  void givenSingletonInstance_whenGetInstanceAgain_thenReturnsSameInstance() {
+    // Given
+    TranslationResource firstInstance = TranslationResource.getInstance(mockModelManager, true);
+
+    // When
+    TranslationResource secondInstance = TranslationResource.getInstance(mockModelManager, false);
+
+    // Then
+    MatcherAssert.assertThat(secondInstance, Matchers.sameInstance(firstInstance));
   }
 }
